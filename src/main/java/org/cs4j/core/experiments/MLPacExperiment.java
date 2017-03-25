3bd9c48409ae7589a8bb1f82f6f984e7ef4451aa
Gal <Dreiman>
2017-03-25 17:33:40 +0300

    update ML-PAC experiment

--- src/main/java/org/cs4j/core/experiments/MLPacExperiment.java
+++ src/main/java/org/cs4j/core/experiments/MLPacExperiment.java
@@ -1,5 +1,6 @@
 package org.cs4j.core.experiments;
 
+import org.apache.log4j.Logger;
 import org.cs4j.core.algorithms.pac.AnytimePTSForMLPac;
 import org.cs4j.core.algorithms.pac.PACSearchFramework;
 import org.cs4j.core.algorithms.pac.conditions.MLPacCondition;
@@ -7,12 +8,15 @@ import org.cs4j.core.domains.VacuumRobot;
 
 public class MLPacExperiment extends StandardExperiment {
 
+	private final static Logger logger = Logger.getLogger(MLPacExperiment.class);
+	
 	public MLPacExperiment() {
 		super(new PACSearchFramework());
-
+		logger.info("Init ML-PAC Experiment");
 		PACSearchFramework psf = (PACSearchFramework) this.searchAlgorithm;
 		psf.setAnytimeSearchClass(AnytimePTSForMLPac.class);
 		psf.setPACConditionClass(MLPacCondition.class);
+		psf.setAdditionalParameter("anytimeSearch", AnytimePTSForMLPac.class.getName());
 	}
 
 	@SuppressWarnings("rawtypes")
@@ -23,9 +27,8 @@ public class MLPacExperiment extends StandardExperiment {
 		double[] epsilons = {0.2};
 		double[] deltas = {0.1};
 
-		PACSearchFramework psf = new PACSearchFramework();
-		psf.setAdditionalParameter("anytimeSearch", AnytimePTSForMLPac.class.getName());
-		Experiment experiment = new StandardExperiment(psf);
+		
+		Experiment experiment = new MLPacExperiment();
 
 		PACOnlineExperimentRunner runner = new PACOnlineExperimentRunner();
 		runner.runExperimentBatch(domains, pacConditions, epsilons, deltas, experiment);
