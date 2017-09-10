aa5b438e3f851f9a08b8d18e02c14fd2de634f98
Gal <Dreiman>
2017-09-10 16:38:13 +0300

    Ratio Based for hard problems full experiment - updates

--- src/main/java/org/cs4j/core/generators/PancakesGenerator.java
+++ src/main/java/org/cs4j/core/generators/PancakesGenerator.java
@@ -134,8 +134,8 @@ public class PancakesGenerator extends GeneralInstancesGenerator {
         int size;
         String previousInstancesDir = null;
         int previousInstancesCount = 0;
-        int pancakesNumLow = 40;
-        int pancakesNumHigh = 40;
+        int pancakesNumLow = 35;
+        int pancakesNumHigh = 35;
 
 
         if(args.length == 2){
